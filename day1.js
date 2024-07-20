827e75e3d08f57356472f12542f04268aabda7ba
RohitYadav34980 <yadav.rohit.nitkrr@gmail.com>
2024-07-20 22:28:25 +0530

    added day2.js

---
+++ day1.js
@@ -0,0 +1,15 @@
+//Activity 1:Variable Declaration       
+
+//Task1:Declare a variable using 'var',assign it a  number ,and log the value to the console
+
+var num =76;
+console.log(num);//output:76
+
+//Task 2:Declare the variable using 'let',adsign it is a string ,and log the  value to the console.
+
+let str="Hello everyone ,muskan this side";
+console.log(str);//output:Hello everyone ,muskan this side
+
+//Activity 2: Constant declartion
+
+ //Task 3: Declare the variable using 'const',assign it is a boolean value ,and log  
---
+++ day2.js
@@ -0,0 +1,152 @@
+ //activity 1
+//  tast 1:write a program to add two numbers and log the result to console.
+ 
+let a=2;
+let b=3;
+console.log(a+b);//output:5
+
+//task 2:write a program to subtract two numbers and log the result to console
+
+a=5;
+b=3;
+console.log(a-b);//output:2
+
+//task 3:write a program to multiply two numbers and log the result to console.
+
+a=5;
+b=4;
+console.log(a*b);//output:20
+
+
+//task 4:write a program to divide two numbers and log the result to console.
+
+a=15;
+b=3;
+console.log(a/b);//output:5
+
+//task 5:write a program to find the remainder when one number is divided by other and log the esult to console
+
+a=14;
+b=5;
+console.log(a%b);//output:4
+
+//Activity 2:Assignment Operations
+
+//task 6:  Use the += operator to add a number to variable and log the result to console.
+
+let c=5;
+c+=3;
+console.log(c);//output:8
+
+//task 7:Use the += operator to add a number to variable and log the result to console.
+
+let d=5;
+d-=2;
+console.log(d);//output:3
+
+//Activity 3:Comparison Operator
+
+//task 8:write a program to compare two numbers using < and > operator and log the result to console.
+
+
+a=5;
+b=3;
+
+console.log(a>b);//output:true
+
+console.log(a<b);//output:false
+
+//task 9: write a program to compare two numbers using <= and >= operator and log the result to console.
+
+
+console.log(a>=b);//output:true
+comnsole.log(a<=b);//output:false
+
+//task 10:write a program to compare two numbers using == and === operator and log the result to console.
+
+a=6;
+b=4;
+console.log(a==b);//output:false
+console.log(a===b);//output:false
+
+
+//Activity 4:Logical opertor
+
+//task 11:Write a program that uses && operator to combine two conditions and log the result to console.
+
+a=7;
+b=9;
+c=5;
+d=4;
+
+console.log((a<b)&&(c<d));//output:false
+
+//task 12: Write a program that uses || operator to combine two conditions and log the result to console.
+    
+
+console.log((a<b)||(c<d));//output:true
+
+
+//task 13:write a program that uses ! operator to negate the condition and log the result to console.
+
+console.log(!(a<b));//output:false
+
+//Activity 5: Ternary Operatos
+
+//task 14:write a program that uses ternary operator to check if a number is positive or negative and log the result to console.
+
+let num =-5;
+let result=(num>=0 ? "positive":"negative");
+console.log(result);//output:negative
+
+//feature request:
+//1  arithmetc operation script 
+
+let n=8;
+
+//addition operator
+console.log(n+2);//output:10
+
+
+
+//substration operator
+console.log(n-2);//output:6
+
+//multiplication operator
+console.log(n*2);//output:16
+
+//division operator
+console.log(n/2);//output:4
+
+//reminder operato
+console.log(n%4);//output:0
+
+//increment operator
+console.log(++n);//output:9
+
+//decrement operator
+console.log(--n);//output:8
+
+//exponentiation operator
+console.log(n**3);//output:512
+
+
+//2 comparison and logical operators script
+
+//equal to operator
+console.log(17==17);//output:true
+
+//not equal to operator 
+console.log(18=10);//output:false
+
+//strictly equal to operator
+console.log(2==='2');//output:false
+
+//greater than operator
+console.log(45>6);//output:true
+
+//less than operator
+console.log(3<8);//output:true
+
+//logical and
+console.log((n<5)&&(n<6));//output:false
